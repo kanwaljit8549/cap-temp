8eb8438bad1000a7b36abfae3dfe95327b950c84
Kanwaljit Singh Khattra <kanwaljit-singh.khattra@capgemini.com>
2025-08-20 13:59:35 +0000

    mbt build

---
+++ app/cap-temp-ui/dist/controller/View1.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"],e=>{"use strict";return e.extend("captempui.controller.View1",{onInit(){let e=this.getOwnerComponent().getModel();console.log(e);this.getView().bindElement({path:"/Books('123')",parameters:{}})}})});
+//# sourceMappingURL=View1.controller.js.map
